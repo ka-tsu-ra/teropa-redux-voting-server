c2154c9a42c2c7594599b6c34d2a317ac9e1bc2d
katsuraku <kirsten.jones@ft.com>
2016-03-04 22:52:51 +0000

    Entries get converted into an immutable structure so the state can be immutable

--- src/core.js
+++ src/core.js
@@ -1,3 +1,6 @@
+import {List} from 'immutable';
+
+// return the result of setting state with an immutable List structure made out of whatever is passed in as entries
 export function setEntries (state, entries) {
-  return state.set('entries', entries);
+  return state.set('entries', List(entries));
 }
