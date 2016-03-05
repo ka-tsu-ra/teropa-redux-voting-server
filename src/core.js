73f959a61c5c699890bdd9d69bc61568c4d4b4a1
katsuraku <kirsten.jones@ft.com>
2016-03-05 09:55:46 +0000

    Can select the first two entries to be voted on and have them in a separate place in the state

--- src/core.js
+++ src/core.js
@@ -1,6 +1,15 @@
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 // return the result of setting state with an immutable List structure made out of whatever is passed in as entries
 export function setEntries (state, entries) {
   return state.set('entries', List(entries));
 }
+
+export function next(state) {
+  const entries = state.get('entries');
+  return state.merge({
+    vote: Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  });
+}
+
