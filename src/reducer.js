80a7a846adcebb9fdf5a005528a90ade5339850c
katsuraku <kirsten.jones@ft.com>
2016-03-06 09:33:38 +0000

    Reducer function accepts setEntries as an action and runs the setEntries function when reducer is called with that action type and params

---
+++ src/reducer.js
@@ -0,0 +1,8 @@
+import {setEntries} from './core';
+
+export default function reducer(state, action) {
+  switch (action.type) {
+    case 'SET_ENTRIES':
+      return setEntries(state, action.entries);
+  }
+}
