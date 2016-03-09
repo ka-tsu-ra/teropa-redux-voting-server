23a739091a6cd91064c244808366c114f5dc82ab
katsuraku <kirsten.jones@ft.com>
2016-03-09 19:51:11 +0000

    Adds test entries into the store to play with, with store.dispatch in index.js. Start vote by dispatching 'NEXT' action to the store.'

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ export const store = makeStore();
 // Provide a function that the store will call after every action it applies, when the state has potentially changed.
 // Do this in startServer function. So need to give it the Redux store.
 startServer(store);
+
+// Load some test entries into the store from a test file. Dispatch the NEXT action to start the vote.
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
